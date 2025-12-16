53359a2d05735a5fc7afaeb0ec3b827cbe354c7c
Zhyar <Zhiyar.majeed@ukh.edu.krd>
2025-12-16 18:57:53 +0300

    updated version

--- Git.java
+++ Git.java
@@ -1,5 +1,6 @@
 public class Git {
     public static void main(String[] args) {
         System.out.println("With great power comes great responsibility");
+        System.out.println("What a quote ha");
     }
 }
